b767f3661e731f1f8c09c99eb7abc86435e15fd1
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2022-10-12 11:05:52 -0300

    [FEAT] Add null injectable in ng form on child

--- src/app/display-contador/display-contador.component.ts
+++ src/app/display-contador/display-contador.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { ControlContainer, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-display-contador',
   templateUrl: './display-contador.component.html',
   styleUrls: ['./display-contador.component.less'],
   encapsulation: ViewEncapsulation.None,
+  viewProviders: [{
+    provide: ControlContainer,
+    useFactory: ControlContainer,
+    deps: [[new Optional(), NngForm]]
+  }]
 })
 export class DisplayContadorComponent implements OnInit {
   @Input()
